09c57e01994f164419c62d14194195a803a642f8
RonewaErnest <ernest.mudau@sambeconsulting.com>
2025-02-18 11:50:17 +0200

    Update insert.sql

--- Database_administration/CRUD_Operations/insert.sql
+++ Database_administration/CRUD_Operations/insert.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO users (name, email, age)  
+VALUES ('Ernest', 'ernest.mudau@sambeconsulting.com', 24),  
+       ('Andile', 'andile.ntumba@sambeconsulting.com', 95);
+       ('Ongeziwe','ongeziwe.cwesi@sambeconsulting.com', 85),
+       ('kiaan','kiaan.patel@sambeconsulting.com', 46),
+       ('thabang','thabang.mothapo@sambeconsulting.com', 67),
