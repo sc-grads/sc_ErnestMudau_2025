1b7cb60b50f5a43447d7fc40a1b9bf97a81bd161
RonewaErnest <ernest.mudau@sambeconsulting.com>
2025-03-09 20:01:16 +0200

    Update insert_data_script.sql

--- Autotest/insert_data_script.sql
+++ Autotest/insert_data_script.sql
@@ -2,5 +2,9 @@ USE AutoTest;
 GO
 
 EXEC InsertUser 'John', 'Doe', 'john.doe@example.com';
+EXEC InsertUser 'Jane', 'Smith', 'jane.smith@example.com';
+EXEC InsertUser 'Bob', 'Johnson', 'bob.johnson@example.com';
+EXEC InsertUser 'Alice', 'Williams', 'alice.williams@example.com';
+EXEC InsertUser 'Charlie', 'Brown', 'charlie.brown@example.com';
 GO
 w
