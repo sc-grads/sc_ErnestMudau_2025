b95fbe314ebbb8589aac320aae7d82f81fe640f5
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-03-12 15:31:29 +0200

    truncate file

---
+++ Database_Administration/SQLstatements/TruncateTable.sql
@@ -0,0 +1,15 @@
+USE [AdventureWorks2022]
+GO
+
+INSERT INTO [dbo].[EmployeeNew]
+           ([EmployeeName])
+     VALUES
+           ('jagadish')
+GO
+
+
+select * from [dbo].[EmployeeNew]
+
+
+truncate table [dbo].[EmployeeNew]
+
