6f0ecca8d9fd52692ed855870396f0503586bcb5
RonewaErnest <ernest.mudau@sambeconsulting.com>
2025-03-26 07:27:46 +0200

    Delete scripts directory

--- scripts/insert_data.sql
+++
@@ -1,6 +0,0 @@
-USE AutoTestDB;
-
--- Insert sample data using the stored procedure
-EXEC InsertUser 'John', 'Doe', 'john.doe@example.com';
-EXEC InsertUser 'Jane', 'Smith', 'jane.smith@example.com';
-EXEC InsertUser 'Bob', 'Johnson', 'bob.johnson@example.com';
