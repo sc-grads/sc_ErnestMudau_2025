3f894097095289cf66cfa983885a75799768c1f7
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-05-05 13:28:07 +0200

    python scripts'strings and type concersion'

---
+++ Python/TypeConversion.py
@@ -0,0 +1,6 @@
+txt_value: str ='100'
+int_value: int = 50
+
+print(int(txt_value)+ int_value)
+
+print(txt_value+ str(int_value))
---
+++ Python/strings.py
@@ -0,0 +1,14 @@
+#string characters
+
+name: str = 'Ronewa'
+fruit: str = 'banana'
+
+print(name  + ' eats a ' + fruit)
+
+poem: str = """
+qwersfgs
+sfgsdg
+xcfhdf
+zfggdf
+"""
+print(poem)
