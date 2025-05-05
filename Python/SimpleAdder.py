135e7d83ac219fd46589b36a579f03cbee13a763
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-05-05 13:33:50 +0200

    SimpleAdder

---
+++ Python/SimpleAdder.py
@@ -0,0 +1,7 @@
+print('Welcome to your simple adder')
+print('----------------------------')
+
+a: str = input('Enter a value for "a":  ')
+b: str = input('Enter a value for "b":  ')
+print('----------------------------')
+print( 'the result is:',int(a) + int(b))
