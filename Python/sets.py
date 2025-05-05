683c1ff64473a1aae9ad264142847ad49da31de4
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-05-05 14:26:17 +0200

    sets and tuples

---
+++ Python/sets.py
@@ -0,0 +1,8 @@
+elements: set = {99, True , 'Ernest'}
+print(elements)
+print('-------------')
+elements.add('erny')
+print(elements)
+print('----------')
+#elements.remove('erny')
+#elements.clear
---
+++ Python/tuples.py
@@ -0,0 +1,4 @@
+#is used for never changing data
+items: tuple = (1,)
+coordinates: tuple[float] = 1.5 ,2.5
+print(coordinates)
