9b4e9810d4604b42a692b64c0e31a3f2d9dd3305
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-05-06 07:34:19 +0200

    If elif else

---
+++ Python/If_Elif_Else.py
@@ -0,0 +1,19 @@
+age : int = 30
+
+if age >= 21:
+    print('You may enter the club')
+else:
+    print('you are not allowed to enter')
+print('--------------------------')
+
+
+#if else shorthand
+
+number: int = 0
+
+if number > 0:
+    result: str =('Above 0')
+else:
+    result: str = '0 and below'
+
+print(result)
