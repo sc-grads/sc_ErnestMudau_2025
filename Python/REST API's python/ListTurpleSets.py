2f600a06790b96f68ea75ff859e58143be517670
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-06-24 10:27:57 +0200

    sets,tuple and lists

---
+++ Python/REST API's python/ListTurpleSets.py
@@ -0,0 +1,11 @@
+l = ["Ernest" ,"happy" , "vero"] #list 
+t = ("Ernest" ,"happy" , "vero") # turple cannot be modified
+s = {"Ernest" ,"happy" , "vero"} # you cant have duplicates
+
+print(t[0]) 
+
+l.append("Ronewa")
+
+print(l)
+s.add("Ronewa")
+print(s)
---
+++ Python/REST API's python/boolean.py
@@ -0,0 +1,10 @@
+print(5==5)
+print(5 > 5)
+print(10 != 10)
+
+#on lists
+friends = {"Bob", "Rolf", "Anne"}
+abroad = {"Bob", "Anne"}
+
+print(friends == abroad)
+
