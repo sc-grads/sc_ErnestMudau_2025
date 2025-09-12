07f13d6c9bcf3f94074742a5cafa7840cfd7c439
Ernest Mudau <ernest.mudau@sambeconsulting.com>
2025-09-12 09:55:11 +0200

    Final API project

---
+++ Database_Administration/PostgreSQL/api/social-repo/migrations/1757577733199_add-users-table.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        bio VARCHAR(400),
+        username VARCHAR(30) NOT NULL
+        );
+    `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`
+        DROP TABLE users`
+    )};
